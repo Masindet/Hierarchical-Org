a940623d4c0e6621c756f8984b7c845caba601a4
Masindet <mmelissagathoni@gmail.com>
2025-07-24 12:20:34 +0300

    minor edits

--- lib/tree_org_web/live/tree_test_live.ex
+++ lib/tree_org_web/live/tree_test_live.ex
@@ -102,19 +102,19 @@ defmodule TreeOrgWeb.TreeTestLive do
   def render_tree(assigns) do
     ~H"""
     <div class="flex flex-col items-center relative">
-      <div class="tree-node cursor-pointer px-4 py-2 border border-gray-300 bg-blue-100 text-sm rounded shadow-sm inline-block mb-2">
+      <div class="tree-node cursor-pointer px-4 py-4 border border-gray-300 bg-blue-100 text-sm rounded shadow-sm inline-block mb-3">
         <%= @node.name %>
       </div>
 
       <%= if @node.children != [] do %>
         <!-- vertical line from parent to children -->
-        <div class="h-8 border-l-2 border-gray-300"></div>
+        <div class="h-6 tree-line relative"></div>
 
         <!-- horizontal connectors and children -->
         <div class="flex justify-center space-x-4 relative">
-        
+
           <!-- Horizontal line connecting children -->
-          <div class="absolute top-26 left-6 w-full border-t-2 border-gray-30 z-0"></div>
+          <div class="absolute top-4 w-full border-t-2 border-gray-30"></div>
 
           <!-- Child nodes -->
           <%= for child <- @node.children do %>
